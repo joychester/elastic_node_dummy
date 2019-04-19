324bb46df774897736d273b7f7ab69adc363380c
Cheng Chi (@joychester) <joychester@gmail.com>
2019-04-19 00:52:30 -0700

    Create elastic_connect.js

---
+++ elastic_connect.js
@@ -0,0 +1,10 @@
+const { Client } = require('@elastic/elasticsearch')
+
+const client = new Client({
+  node: 'http://localhost:9200',
+  maxRetries: 5,
+  requestTimeout: 60000,
+  sniffOnStart: true
+})
+
+module.exports = client;
