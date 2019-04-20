ea63ca6905b23343b5236dab801cdcb11aed78ba
Cheng Chi (@joychester) <joychester@gmail.com>
2019-04-19 22:39:20 -0700

    Update elastic_connect.js

--- elastic_connect.js
+++ elastic_connect.js
@@ -2,8 +2,8 @@ const { Client } = require('@elastic/elasticsearch')
 
 const client = new Client({
   node: 'http://localhost:9200',
-  maxRetries: 5,
-  requestTimeout: 60000,
+  maxRetries: 3,
+  requestTimeout: 30000,
   sniffOnStart: true
 })
 
